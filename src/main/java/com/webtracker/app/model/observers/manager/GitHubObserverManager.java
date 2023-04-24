434892ed62a2bf5b4b738f00be3a6a522db66249
aniaxb <a.banachowicz11@gmail.com>
2023-04-24 09:27:13 +0200

    Removed DESCRIPTION_CHANGE enum

--- src/main/java/com/webtracker/app/model/observers/manager/GitHubObserverManager.java
+++ src/main/java/com/webtracker/app/model/observers/manager/GitHubObserverManager.java
@@ -12,15 +12,15 @@ import java.util.*;
 public class GitHubObserverManager extends ObserverManager<GitHubState, GitHubObserverManager.GitHubConsideredActions> {
 
     public enum GitHubConsideredActions {
-        REPOSITORY_CHANGE, COMMIT_CHANGE, DESCRIPTION_CHANGE
+        REPOSITORY_CHANGE, COMMIT_CHANGE
     }
 
     // what actions to consider
     private static final Set<GitHubConsideredActions> setOfActions = Set.of(
             GitHubConsideredActions.REPOSITORY_CHANGE,
-            GitHubConsideredActions.COMMIT_CHANGE,
-            GitHubConsideredActions.DESCRIPTION_CHANGE);
+            GitHubConsideredActions.COMMIT_CHANGE);
 
+    //define interesting languages to observe repositories with that language
     private static final Set<CodingLanguage> codingLanguages = new HashSet<>(Collections.singleton(CodingLanguage.JAVA));
 
     // what observers for each action
