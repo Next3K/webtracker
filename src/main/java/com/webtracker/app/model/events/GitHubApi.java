62202604f9c812599a36089868bf17df2f4cbaa3
JMielczarek02 <102308814+JMielczarek02@users.noreply.github.com>
2023-05-16 22:13:13 +0200

    add commit info method

--- src/main/java/com/webtracker/app/model/events/GitHubApi.java
+++ src/main/java/com/webtracker/app/model/events/GitHubApi.java
@@ -1,13 +1,18 @@
 package com.webtracker.app.model.events;
 
+import com.webtracker.app.model.states.github.GitHubCommit;
 import com.webtracker.app.model.states.github.GitHubOwner;
 import com.webtracker.app.model.states.github.GitHubState;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class GitHubApi {
@@ -30,4 +35,23 @@ public class GitHubApi {
         return Objects.requireNonNull(response).body();
     }
 
+    public static List<GitHubCommit> getCommitsInfo(String user, String repoName){
+        List<GitHubCommit> commitsList = new ArrayList<>();
+        String commitResponse = call("https://api.github.com/repos/"+user+"/"+repoName+"/commits");
+        JSONArray commits = new JSONArray(commitResponse);
+        for (int i = 0; i < commits.length(); i++) {
+            JSONObject commit = commits.getJSONObject(i);
+            String committerName = commit.getJSONObject("commit").getJSONObject("committer").getString("name");
+            String commitMsg = commit.getJSONObject("commit").getString("message");
+            String sha = commit.getString("sha");
+            String url = commit.getString("html_url");
+            GitHubCommit gitHubCommit = new GitHubCommit();
+            gitHubCommit.setCommitterName(committerName);
+            gitHubCommit.setCommitMessage(commitMsg);
+            gitHubCommit.setCommitSha(sha);
+            gitHubCommit.setUrl(url);
+            commitsList.add(gitHubCommit);
+        }
+        return commitsList;
+    }
 }
