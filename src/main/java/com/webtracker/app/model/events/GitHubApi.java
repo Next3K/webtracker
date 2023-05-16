1144dd853b473b39c998767c7f58ab9d66c4193e
JulianWoroniecki <236702@edu.p.lodz.pl>
2023-05-16 23:12:11 +0200

    Add no. of changed lines of code in GitHubCommit, add account description and auto incrementing ID to GitHubState

--- src/main/java/com/webtracker/app/model/events/GitHubApi.java
+++ src/main/java/com/webtracker/app/model/events/GitHubApi.java
@@ -21,16 +21,25 @@ public class GitHubApi {
     private static final String TOKEN = "";
     public static GitHubState callApi(GitHubOwner owner){
         List<GitHubRepository> repositoriesList = new ArrayList<>();
-        String username=owner.username();
-        String repositories = call("https://api.github.com/users/"+username+"/repos");
-        JSONArray repos = new JSONArray(repositories);
-        for (int i = 0; i < repos.length(); i++) {
-            JSONObject repo = repos.getJSONObject(i);
+        String username = owner.username();
+        String repositoriesResponse = call("https://api.github.com/users/"+username+"/repos");
+        JSONArray repositories = new JSONArray(repositoriesResponse);
+        for (int i = 0; i < repositories.length(); i++) {
+            JSONObject repo = repositories.getJSONObject(i);
             String repoName = repo.getString("name");
             repositoriesList.add(getRepoInfo(username,repoName));
         }
-        //Here create new GitHubState and fill it with proper data, then return
-        return new GitHubState();
+        GitHubState gitHubState = new GitHubState();
+        gitHubState.setObservatorEmail(null);
+        gitHubState.setOwner(owner);
+        gitHubState.setGitHubAccountDescription(getAccountDescription(username));
+        gitHubState.setRepositories(repositoriesList);
+        return gitHubState;
+    }
+    public static String getAccountDescription(String username) {
+        String userResponse = call("https://api.github.com/users/"+username);
+        JSONObject user = new JSONObject(userResponse);
+        return user.getString("bio");
     }
     public static String call(String url){
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url))
@@ -46,31 +55,41 @@ public class GitHubApi {
         return Objects.requireNonNull(response).body();
     }
 
-    public static List<GitHubCommit> getCommitsInfo(String user, String repoName){
+    public static List<GitHubCommit> getCommitsInfo(String username, String repoName){
         List<GitHubCommit> commitsList = new ArrayList<>();
-        String commitResponse = call("https://api.github.com/repos/"+user+"/"+repoName+"/commits");
+        String commitResponse = call("https://api.github.com/repos/"+username+"/"+repoName+"/commits");
         JSONArray commits = new JSONArray(commitResponse);
         for (int i = 0; i < commits.length(); i++) {
-            JSONObject commit = commits.getJSONObject(i);
-            String committerName = commit.getJSONObject("commit").getJSONObject("committer").getString("name");
-            String commitMsg = commit.getJSONObject("commit").getString("message");
-            String sha = commit.getString("sha");
-            String url = commit.getString("html_url");
-            GitHubCommit gitHubCommit = new GitHubCommit();
-            gitHubCommit.setCommitterName(committerName);
-            gitHubCommit.setCommitMessage(commitMsg);
-            gitHubCommit.setCommitSha(sha);
-            gitHubCommit.setUrl(url);
+            JSONObject commitObject = commits.getJSONObject(i);
+            if (commitObject.has("author") && !commitObject.isNull("author")) {
+                String authorName = commitObject.getJSONObject("author").getString("login");
+                if (!authorName.equals(username)) continue;
+            }
+            else {
+                continue;
+            }
+            String committerName = commitObject.getJSONObject("commit").getJSONObject("committer").getString("name");
+            String commitMsg = commitObject.getJSONObject("commit").getString("message");
+            String sha = commitObject.getString("sha");
+            String html_commmit_url = commitObject.getString("html_url");
+            String api_commit_url = commitObject.getString("url");
+
+            String individualCommitResponse = call(api_commit_url);
+            JSONObject individualCommitObject = new JSONObject(individualCommitResponse);
+            int addedLines = individualCommitObject.getJSONObject("stats").getInt("additions");
+            int deletedLines = individualCommitObject.getJSONObject("stats").getInt("deletions");
+
+            GitHubCommit gitHubCommit = new GitHubCommit(committerName, commitMsg, sha, html_commmit_url, addedLines, deletedLines);
             commitsList.add(gitHubCommit);
         }
         return commitsList;
     }
 
-    public static GitHubRepository getRepoInfo(String user, String repoName){
+    public static GitHubRepository getRepoInfo(String username, String repoName){
         GitHubRepository gitHubRepository = new GitHubRepository();
-        String repoResponse = call("https://api.github.com/repos/"+user+"/"+repoName);
-        JSONObject repositoryObject=new JSONObject(repoResponse);
-        String languagesResponse = call("https://api.github.com/repos/"+user+"/"+repoName+"/languages");
+        String repositoryResponse = call("https://api.github.com/repos/"+username+"/"+repoName);
+        JSONObject repositoryObject = new JSONObject(repositoryResponse);
+        String languagesResponse = call("https://api.github.com/repos/"+username+"/"+repoName+"/languages");
         JSONObject languagesObject = new JSONObject(languagesResponse);
         List<String> languages = new ArrayList<>(languagesObject.keySet());
         List<CodingLanguage> enumLanguages = new ArrayList<>();
@@ -90,8 +109,8 @@ public class GitHubApi {
         gitHubRepository.setDescription(repositoryObject.get("description").toString());
         gitHubRepository.setRepositoryID(repositoryObject.getInt("id"));
         gitHubRepository.setUrl(repositoryObject.getString("html_url"));
-        gitHubRepository.setCodingLanguages(null);
-        gitHubRepository.setCommits(getCommitsInfo(user, repoName));
+        gitHubRepository.setCodingLanguages(enumLanguages);
+        gitHubRepository.setCommits(getCommitsInfo(username, repoName));
         return gitHubRepository;
     }
 
