d512ca8b3c2d479329b26aa5b62289da93a084c4
JMielczarek02 <102308814+JMielczarek02@users.noreply.github.com>
2023-05-16 22:08:04 +0200

    add call API method

--- src/main/java/com/webtracker/app/model/events/GitHubApi.java
+++ src/main/java/com/webtracker/app/model/events/GitHubApi.java
@@ -3,6 +3,13 @@ package com.webtracker.app.model.events;
 import com.webtracker.app.model.states.github.GitHubOwner;
 import com.webtracker.app.model.states.github.GitHubState;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Objects;
+
 public class GitHubApi {
 
     // call git hub api for given username
@@ -10,6 +17,17 @@ public class GitHubApi {
 //        String username = owner.username();
         return new GitHubState();
     }
+    public static String call(String url){
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url))
+                .method("GET",HttpRequest.BodyPublishers.noBody()).build();
 
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return Objects.requireNonNull(response).body();
+    }
 
 }
