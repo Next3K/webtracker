14bd5ef216f26a047886aec5e3009357807d39d3
236660 Strachota Piotr <236660@edu.p.lodz.pl>
2023-05-22 11:10:25 +0200

    comment out unused classes

--- src/main/java/com/webtracker/app/model/events/GitHubEventProvider.java
+++ src/main/java/com/webtracker/app/model/events/GitHubEventProvider.java
@@ -1,19 +1,19 @@
-package com.webtracker.app.model.events;
-
-import com.webtracker.app.model.states.github.GitHubState;
-
-import java.util.List;
-import org.springframework.stereotype.Service;
-
-@Service
-public class GitHubEventProvider implements EventProvider<GitHubState> {
-    @Override
-    public List<Event> getEvents(GitHubState oldState) {
-        GitHubState newState = getNewState(oldState);
-        return newState.getObserverManager().collectEvents();
-    }
-
-    private GitHubState getNewState(GitHubState oldState) {
-        return GitHubApi.callApi(oldState.getOwner());
-    }
-}
+//package com.webtracker.app.model.events;
+//
+//import com.webtracker.app.model.states.github.GitHubState;
+//
+//import java.util.List;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class GitHubEventProvider implements EventProvider<GitHubState> {
+//    @Override
+//    public List<Event> getEvents(GitHubState oldState) {
+//        GitHubState newState = getNewState(oldState);
+//        return newState.getObserver().getCollectedEvents();
+//    }
+//
+//    private GitHubState getNewState(GitHubState oldState) {
+//        return GitHubApi.callApi(oldState.getOwner().getName());
+//    }
+//}
--- src/main/java/com/webtracker/app/model/jobs/GitHubJob.java
+++ src/main/java/com/webtracker/app/model/jobs/GitHubJob.java
@@ -1,14 +1,14 @@
-package com.webtracker.app.model.jobs;
-
-import com.webtracker.app.model.states.github.GitHubState;
-
-public class GitHubJob extends Job<GitHubState>{
-
-    public GitHubJob(GitHubState gitHubState, int i) {
-        this.setJobTarget(gitHubState);
-        this.setPeriod(i);
-    }
-
-    public GitHubJob() {
-    }
-}
+//package com.webtracker.app.model.jobs;
+//
+//import com.webtracker.app.model.states.github.GitHubState;
+//
+//public class GitHubJob extends Job<GitHubState>{
+//
+//    public GitHubJob(GitHubState gitHubState, int i) {
+//        this.setJobTarget(gitHubState);
+//        this.setPeriod(i);
+//    }
+//
+//    public GitHubJob() {
+//    }
+//}
