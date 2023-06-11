30f0532973ccfb4070b4010e319dc902556baee8
236660 Strachota Piotr <236660@edu.p.lodz.pl>
2023-06-11 20:53:43 +0200

    refactor ScheduledTasks

--- src/main/java/com/webtracker/app/scheduling/ScheduledTasks.java
+++ src/main/java/com/webtracker/app/scheduling/ScheduledTasks.java
@@ -1,16 +1,17 @@
 package com.webtracker.app.scheduling;
 
+import com.webtracker.app.mail.EmailSenderServiceImpl;
 import com.webtracker.app.model.events.Event;
+import com.webtracker.app.model.github.GitHubState;
 import com.webtracker.app.model.observers.GitHubReposObserver;
 import com.webtracker.app.model.observers.Observer;
-import com.webtracker.app.model.github.GitHubState;
 import com.webtracker.app.repository.EventRepository;
 import com.webtracker.app.repository.GitHubObserverRepository;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,14 +21,18 @@ import java.util.stream.Collectors;
 
 
 @Component
+@RequiredArgsConstructor
 public class ScheduledTasks {
 
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+    private final GitHubObserverRepository gitHubObserverRepository;
+    private final EventRepository eventRepository;
+    private final EmailSenderServiceImpl emailSenderService;
 
     @Scheduled(fixedRate = 60_000) // check stats every 1 minute
     public void detectEvents() {
         log.info("Started updating observers");
-        List<GitHubReposObserver> observerList = GitHubObserverRepository.getAll();
+        List<GitHubReposObserver> observerList = gitHubObserverRepository.findAll();
 
         // which repos to which observers
         Map<GitHubState, List<GitHubReposObserver>> collect =
@@ -53,15 +58,18 @@ public class ScheduledTasks {
             }
         }
 
-        EventRepository.addAll(events);
+        eventRepository.saveAll(events);
         log.info("Finished updating observers");
     }
 
     @Scheduled(fixedRate = 300_000) // send events every 5 minutes
     public void sendEvents() {
         log.info("Started Sending Events");
-        List<Event> eventsToSend = EventRepository.popAll();
-        // TODO use sender to send events (mail)
+        List<Event> eventsToSend = eventRepository.findAll();
+        eventsToSend.forEach(
+                event -> emailSenderService.sendEmail(event.getEmailToSendEvent(), event.getEventTitle(),
+                        event.getEventDescription() + "\n" + event.getLink())
+        );
         log.info("Finished Sending Events");
     }
 }
