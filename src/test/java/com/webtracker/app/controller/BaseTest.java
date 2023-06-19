0e0eacb9a6517e490823b4a7d906457ee5dbba9e
236660 Strachota Piotr <236660@edu.p.lodz.pl>
2023-06-19 14:39:12 +0200

    add base test

---
+++ src/test/java/com/webtracker/app/controller/BaseTest.java
@@ -0,0 +1,31 @@
+package com.webtracker.app.controller;
+
+import jakarta.transaction.Transactional;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@Testcontainers
+class BaseTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER =
+            new PostgreSQLContainer<>("postgres:15")
+                    .withDatabaseName("webtracker")
+                    .withUsername("postgres")
+                    .withPassword("postgres");
+
+    @DynamicPropertySource
+    public static void containerConfig(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRE_SQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRE_SQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRE_SQL_CONTAINER::getPassword);
+    }
+}
