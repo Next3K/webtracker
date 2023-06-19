5c528a8255890426a4fec1c6efdc209554b72255
236660 Strachota Piotr <236660@edu.p.lodz.pl>
2023-06-19 14:39:36 +0200

    implement custom exceptions

--- src/main/java/com/webtracker/app/controllers/TrackingController.java
+++ src/main/java/com/webtracker/app/controllers/TrackingController.java
@@ -3,18 +3,21 @@ package com.webtracker.app.controllers;
 import com.webtracker.app.dto.DeleteUserDto;
 import com.webtracker.app.dto.TrackUserDto;
 import com.webtracker.app.dto.mapper.ClientMapper;
-import com.webtracker.app.model.events.GitHubApi;
 import com.webtracker.app.model.github.GitHubOwner;
 import com.webtracker.app.model.github.GitHubState;
 import com.webtracker.app.model.observers.GitHubCommitObserver;
 import com.webtracker.app.model.observers.GitHubReposObserver;
 import com.webtracker.app.model.observers.ObserverType;
 import com.webtracker.app.repository.GitHubCommitObserverRepository;
-import com.webtracker.app.repository.GitHubRepoObserverRepository;
 import com.webtracker.app.repository.GitHubOwnerRepository;
+import com.webtracker.app.repository.GitHubRepoObserverRepository;
+import com.webtracker.app.repository.GithubStateRepository;
+import com.webtracker.app.service.GitHubApi;
+import com.webtracker.app.exceptions.WrongObserverTypeException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -25,6 +28,7 @@ public class TrackingController {
     private final GitHubOwnerRepository gitHubOwnerRepository;
     private final GitHubRepoObserverRepository gitHubRepoObserverRepository;
     private final GitHubCommitObserverRepository gitHubCommitObserverRepository;
+    private final GithubStateRepository githubStateRepository;
 
     /**
      * Clients adds tracking task by specifying GitHub user to track and what to track
@@ -43,26 +47,20 @@ public class TrackingController {
             gitHubOwnerRepository.save(byUsername);
         }
 
-        GitHubState initialState = GitHubApi.callApi(byUsername);
-
-        if (trackUserDto.getObserverType() == ObserverType.GitHubReposObserver) {
-            GitHubReposObserver gitHubReposObserver =
-                    new GitHubReposObserver(trackUserDto.getTechnologies(), initialState, ClientMapper.mapToClient(trackUserDto.getClient()));
-
+        if (trackUserDto.getObserverType().equals(ObserverType.GitHubReposObserver.getName())) {
+            GitHubState initialState = GitHubApi.callApi(byUsername);
+            GitHubReposObserver gitHubReposObserver = new GitHubReposObserver(trackUserDto.getTechnologies(), initialState, ClientMapper.mapToClient(trackUserDto.getClient()));
             gitHubRepoObserverRepository.save(gitHubReposObserver);
+            return ResponseEntity.status(201).body("GitHub user: + " + trackUserDto.getGithubUsername() + " is now being tracked");
 
-            return ResponseEntity.status(201)
-                    .body("GitHub user: + " + trackUserDto.getGithubUsername() + " is now being tracked");
-        } else if (trackUserDto.getObserverType() == ObserverType.CommitsObserver) {
-            GitHubCommitObserver gitHubCommitObserver =
-                    new GitHubCommitObserver(trackUserDto.getTechnologies(), initialState, ClientMapper.mapToClient(trackUserDto.getClient()));
-
+        } else if (trackUserDto.getObserverType().equals(ObserverType.CommitsObserver.getName())) {
+            GitHubState initialState = GitHubApi.callApi(byUsername);
+            GitHubCommitObserver gitHubCommitObserver = new GitHubCommitObserver(trackUserDto.getTechnologies(), initialState, ClientMapper.mapToClient(trackUserDto.getClient()));
             gitHubCommitObserverRepository.save(gitHubCommitObserver);
 
-            return ResponseEntity.status(201)
-                    .body("GitHub user: + " + trackUserDto.getGithubUsername() + " is now being tracked");
+            return ResponseEntity.status(201).body("GitHub user: + " + trackUserDto.getGithubUsername() + " is now being tracked");
         } else {
-            throw new RuntimeException("Observer type not supported");
+            throw new WrongObserverTypeException("Observer type not supported");
         }
 
     }
@@ -74,10 +72,21 @@ public class TrackingController {
      * @return Status 201
      */
     @DeleteMapping()
-    public ResponseEntity<String> stopTrackingGitHubUser(
-            @Valid @RequestBody DeleteUserDto deleteUserDto) {
-        return ResponseEntity.status(204)
-                .body("User + " + deleteUserDto.getGithubUsername() + " removed from track");
+    @Transactional
+    public ResponseEntity<String> stopTrackingGitHubUser(@Valid @RequestBody DeleteUserDto deleteUserDto) {
+
+        GitHubOwner byUsername = gitHubOwnerRepository.findByUsername(deleteUserDto.getGithubUsername());
+        GitHubState stateToDelete = githubStateRepository.findByOwner(byUsername);
+
+        if (deleteUserDto.getObserverType().equals(ObserverType.GitHubReposObserver.getName())) {
+            gitHubRepoObserverRepository.deleteByOldState(stateToDelete);
+        } else if (deleteUserDto.getObserverType().equals(ObserverType.CommitsObserver.getName())) {
+            gitHubCommitObserverRepository.deleteByOldState(stateToDelete);
+        } else {
+            throw new WrongObserverTypeException("Observer type not supported");
+        }
+
+        return ResponseEntity.status(204).body("User + " + deleteUserDto.getGithubUsername() + " removed from track");
     }
 
 
---
+++ src/main/java/com/webtracker/app/exceptions/WrongObserverTypeException.java
@@ -0,0 +1,9 @@
+package com.webtracker.app.exceptions;
+
+public class WrongObserverTypeException extends RuntimeException {
+
+    public WrongObserverTypeException(String message) {
+        super(message);
+    }
+
+}
