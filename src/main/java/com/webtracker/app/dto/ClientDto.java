4a44640174df8c368385edded3faaa91bb058eb8
236660 Strachota Piotr <236660@edu.p.lodz.pl>
2023-06-20 13:25:23 +0200

    add builder annotations

--- src/main/java/com/webtracker/app/dto/ClientDto.java
+++ src/main/java/com/webtracker/app/dto/ClientDto.java
@@ -1,12 +1,10 @@
 package com.webtracker.app.dto;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
+@Builder
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/com/webtracker/app/dto/DeleteUserDto.java
+++ src/main/java/com/webtracker/app/dto/DeleteUserDto.java
@@ -4,13 +4,12 @@ import com.webtracker.app.model.observers.ObserverType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class DeleteUserDto {
